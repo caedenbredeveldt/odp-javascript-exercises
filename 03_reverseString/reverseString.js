1afb4b18c97baed039e1364b916d2fcde65a41e1
Caeden Dell <61693903+caedenbredeveldt@users.noreply.github.com>
2025-03-06 11:36:19 +0200

    Final solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,18 +1,5 @@
 const reverseString = function(string) {
-    // space = " ";
-    // if (space in string){
-
-    // }
-    let result = []
-    arrWords = string.split(" ");
-    for (let i = arrWords.length - 1; i >= 0; i--){
-        word = arrWords[i];
-        arr = word.split("");
-        arr.reverse();
-        result.push(arr.join(''));
-    }
-    return result.join(' ');
-    
+    return string.split("").reverse().join("");   
 };
 
 // Do not edit below this line
