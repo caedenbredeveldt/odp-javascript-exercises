ee85e029e5de11ad60e216d35975919de8b39d10
Caeden Dell <61693903+caedenbredeveldt@users.noreply.github.com>
2025-03-06 15:15:34 +0200

    Completed solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,15 +1,14 @@
-const sumAll = function(num1, num2) {
-    if (Number.isInteger(num1) && Number.isInteger(num2) && num1 > 0 && num2 > 0){
-        let sum = 0;
-        if (num1 <= num2){
-            for(let i = num1; i <= num2; i++){
-                sum += i;
-            }
+const sumAll = function(min, max) {
+    if (Number.isInteger(min) && Number.isInteger(max) && min > 0 && max > 0){
+        if (min > max) {
+            const temp = min;
+            min = max;
+            max = temp;
         }
-        else{
-            for(let i = num2; i <= num1; i++){
-                sum += i;
-            }
+
+        let sum = 0;
+        for(let i = min; i <= max; i++){
+            sum += i;
         }
         return sum;
     }
