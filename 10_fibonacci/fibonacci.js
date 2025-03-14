7a435e34f1773be02870de699b2f2f255beeb653
Caeden Dell <61693903+caedenbredeveldt@users.noreply.github.com>
2025-03-14 17:24:04 +0200

    Completed Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,34 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    target = typeof n != 'number' ? parseInt(n) : n;
+    if (target < 0){
+        return 'OOPS';
+    }
+    if (target === 0)
+    {
+        return 0;
+    }
+    if (target == 1){
+        return 1;
+    };
+    let prev = 0
+    let sum = 1;
+    for (let i = 2; i <= target; i++){
+        let temp = sum;
+        sum += prev;
+        prev = temp;
+    }
+    return sum;
 };
+// const fibonacci = function(n) {
+//     if (n === 0)
+//     {
+//         return 0;
+//     }
+//     if (n == 1){
+//         return 1;
+//     };
+//     return fibonacci(n-1)+fibonacci(n-2);
+// };
 
 // Do not edit below this line
 module.exports = fibonacci;
