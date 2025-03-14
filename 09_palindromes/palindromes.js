ab96e9b0837450b151598e6268e745676a3c1fab
Caeden Dell <61693903+caedenbredeveldt@users.noreply.github.com>
2025-03-14 17:06:42 +0200

    Completed exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase();
+    let arr = string.split("");
+    const onlyAlphaNumeric = arr.filter((c) => {
+        code = c.charCodeAt(0);
+        if (!(code > 47 && code < 58) && // numeric (0-9)
+        !(code > 64 && code < 91) && // upper alpha (A-Z)
+        !(code > 96 && code < 123)) { // lower alpha (a-z)
+            return false;
+        }
+        else{
+            return true
+        }
+    }).join("");
+    const rev = onlyAlphaNumeric.split("").reverse().join("");
+    return onlyAlphaNumeric === rev;   
 };
 
 // Do not edit below this line
